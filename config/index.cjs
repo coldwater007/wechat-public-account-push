83b8f5004c6a3bb72cc590d8e64ba62a9ad426aa
coldwater007 <31716812+coldwater007@users.noreply.github.com>
2022-11-24 12:01:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到1	一只老风铃	omvZI67KQ-o-vSvu3UH2OurKFIWQ
-      id: 'omvZI67KQ-o-vSvu3UH2OurKFIWQ',
+      id: 'ddd1062739524ef484ffe5e5e118ec1c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -60,7 +60,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'omvZI67KQ-o-vSvu3UH2OurKFIWQ',
+      id: 'ddd1062739524ef484ffe5e5e118ec1c',
     }
   ],
 
